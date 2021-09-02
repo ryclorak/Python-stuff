96bb3d19772f661ab9924804628e4f5e0018c09c
ryclorak <ryclorak@gmail.com>
2021-09-02 12:26:09 -0700

    Added elif prints
    expanded the conditions under which different text printed after guesses

--- guessGame.py
+++ guessGame.py
@@ -9,27 +9,33 @@ name = input()
 print('Well, ' + name + ', I am thinking of a number between 1 and 20')
 secretNumber = random.randint(1, 20)
 
-print('The number is ' + str(secretNumber))
+##print('The number is ' + str(secretNumber))
 
 for guessesTaken in range(1,7):
     print('Take a guess.')
     guess = int(input())
-# TODO: only print too low/high if guesses remain
-    if guess < secretNumber:
-        print('Too low.')
-    elif guess > secretNumber:
-        print('Too high.')
-    else:
-        break # correct guess
+    if guessesTaken < 6:
+        if guess < secretNumber:
+            print('Too low.')
+        elif guess > secretNumber:
+            print('Too high.')
+        else:
+            break # correct guess
 
 if guess == secretNumber:
+    if guessesTaken<2:
+        print('Wow! ', end='')
     print('Good job ' + name + '!')
 else:
     print('Correct!')
     time.sleep(3)
     print('NOT.')
     time.sleep(1)
-    print('The number I was thinking of was ' + str(secretNumber))
+    print('The number I was thinking of was ' + str(secretNumber) + '.')
 
-
-print('That was ' + ('only ' if guessesTaken<5 else '') + str(guessesTaken)+ ' guesses.')
+if guessesTaken<2:
+    print ('You got it right on the first try!')
+elif guessesTaken>5:
+    print ('You wasted ' + str(guessesTaken) + ' guesses...')
+else:
+    print('That was ' + ('only ' if guessesTaken<4 else '') + str(guessesTaken)+ ' guesses.')
